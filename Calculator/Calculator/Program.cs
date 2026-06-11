4a4e84cf0f948c2eaa134a9c42e67ebdb2421a76
Alfie McCormack <mccoar1@student.op.ac.nz>
2026-06-12 00:03:01 +1200

    Done some binary and all matrices

--- Calculator/Calculator/Program.cs
+++ Calculator/Calculator/Program.cs
@@ -11,7 +11,7 @@ namespace Calculator
             {
                 Console.Clear();
                 Console.WriteLine("Main Menu\nPlease select from the numbers below");
-                Console.WriteLine("1  Basic Calculator (+ - * / % ! ^)\n2  Binary\n3  Task 3\n4  Task 4\n0  Exit Menu System\n");
+                Console.WriteLine("1  Basic Calculator (+ - * / % ! ^ SQRT)\n2  Binary\n3  Matrices\n4  Task 4\n0  Exit Menu System\n");
                 string temp = Console.ReadLine();
                 task = Convert.ToInt32(temp);
                 Console.Clear();
@@ -24,8 +24,9 @@ namespace Calculator
                     case 2:
                         Binary();
                         break;
-                        //case 3:
-                        //    Three();
+                    case 3:
+                        Matrices();
+                        break;
                         //    break;
                         //case 4:
                         //    Four();
@@ -68,10 +69,10 @@ namespace Calculator
             {
                 op = "^";
             }
-            //else if (input.Contains("√"))//Square root
-            //{
-            //    op = "√";
-            //}
+            else if (input.Contains("sqrt"))//Square root
+            {
+                op = "sqrt";
+            }
             else
             {
                 Console.WriteLine("\nInvalid operator.");
@@ -99,6 +100,10 @@ namespace Calculator
                 Console.ReadLine();
                 return;
             }
+            //else if (op == "sqrt")
+            //{
+            //    double numm1 = Convert.ToDouble(input.Replace("sqrt", ""));
+            //}
             string[] numbers = input.Split(op[0]);
             double num1 = Convert.ToDouble(numbers[0]);
             double num2 = Convert.ToDouble(numbers[1]);
@@ -142,7 +147,6 @@ namespace Calculator
                 }
                 Console.WriteLine($"Answer: {result}");
             }
-            //Square root
             Console.WriteLine("\nPress enter to continue.");
             Console.ReadLine();
             return;
@@ -151,7 +155,7 @@ namespace Calculator
         public static void Binary()
         {
             Console.WriteLine("Binary Calculator: ");
-            Console.WriteLine("1. Decimal to Binary\n2. Binary Addition and Subtraction");
+            Console.WriteLine("1. Decimal to Binary\n2. Binary Addition and Subtraction\n3. Hex Binary and Decimal conversion");
             string choice = Console.ReadLine();
             if (choice == "1")
             {
@@ -176,9 +180,6 @@ namespace Calculator
                     binary = "0" + binary;
                 }
                 Console.WriteLine($"Binary: {binary}");
-                Console.WriteLine("\nPress enter to continue.");
-                Console.ReadLine();
-                return;
             }
             else if (choice == "2")
             {
@@ -213,13 +214,124 @@ namespace Calculator
                     num3 = num1 - num2;
                 }
                 string binary = Convert.ToString(num3, 2);
-                
                 while (binary.Length < 8)//Adds 0s so the binary is always 8 bits long
                 {
                     binary = "0" + binary;
                 }
                 Console.WriteLine($"= {binary}");
+                Console.WriteLine("\nPress enter to continue.");
                 Console.ReadLine();
+                return;
+            }
+            //else if (choice == "3")
+            //{
+            //    Console.WriteLine("Binary Calculator: ");
+            //    Console.WriteLine("1. Decimal to Binary/Hex\n2. Binary to Decimal/Hex\n3. Hex to Decimal/Binary");
+            //    Console.WriteLine("\nCalculator: ");
+            //    string choice2 = Console.ReadLine();
+            //    if (choice2 == "1")
+            //    {
+
+            //    }
+            //    else if (choice2 == "2")
+            //    {
+
+            //    }
+            //    else if (choice2 == "3")
+            //    {
+
+            //    }
+            //}
+            else
+            {
+                Console.WriteLine("\nInvalid option");
+                Console.WriteLine("Press enter to continue.");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("\nPress enter to continue.");
+            Console.ReadLine();
+            return;
+
+
+        }
+        public static void Matrices()
+        {
+            Console.WriteLine("Matrices Calculator: ");
+            Console.WriteLine("1. Addition\n2. Subtraction\n3. Dot Product\n4. Scalar\n5. Determinant");
+            string choice = Console.ReadLine();
+            Console.WriteLine("\na: b: c: d:");
+            string input1 = Console.ReadLine();
+            string[] numbers1 = input1.Split(" ");
+            int a = Convert.ToInt32(numbers1[0]);
+            int b = Convert.ToInt32(numbers1[1]);
+            int c = Convert.ToInt32(numbers1[2]);
+            int d = Convert.ToInt32(numbers1[3]);
+
+
+            if (choice == "1")
+            {
+                Console.WriteLine("\ne: f: g: h:");
+                string input2 = Console.ReadLine();
+                string[] numbers2 = input2.Split(" ");
+                int e = Convert.ToInt32(numbers2[0]);
+                int f = Convert.ToInt32(numbers2[1]);
+                int g = Convert.ToInt32(numbers2[2]);
+                int h = Convert.ToInt32(numbers2[3]);
+                int num1 = a + e;
+                int num2 = b + f;
+                int num3 = c + g;
+                int num4 = d + h;
+                Console.WriteLine($"\n{num1} {num2}");
+                Console.WriteLine($"{num3} {num4}");
+            }
+            else if (choice == "2")
+            {
+                Console.WriteLine("\ne: f: g: h:");
+                string input2 = Console.ReadLine();
+                string[] numbers2 = input2.Split(" ");
+                int e = Convert.ToInt32(numbers2[0]);
+                int f = Convert.ToInt32(numbers2[1]);
+                int g = Convert.ToInt32(numbers2[2]);
+                int h = Convert.ToInt32(numbers2[3]);
+                int num1 = a - e;
+                int num2 = b - f;
+                int num3 = c - g;
+                int num4 = d - h;
+                Console.WriteLine($"\n{num1} {num2}");
+                Console.WriteLine($"{num3} {num4}");
+            }
+            else if (choice == "3")
+            {
+                Console.WriteLine("\ne: f: g: h:");
+                string input2 = Console.ReadLine();
+                string[] numbers2 = input2.Split(" ");
+                int e = Convert.ToInt32(numbers2[0]);
+                int f = Convert.ToInt32(numbers2[1]);
+                int g = Convert.ToInt32(numbers2[2]);
+                int h = Convert.ToInt32(numbers2[3]);
+                int num1 = (a * e) + (b * g);
+                int num2 = (a * f) + (b * h);
+                int num3 = (c * e) + (d * g);
+                int num4 = (c * f) + (d * h);
+                Console.WriteLine($"\n{num1} {num2}");
+                Console.WriteLine($"{num3} {num4}");
+            }
+            else if (choice == "4")
+            {
+                Console.WriteLine("Enter scalar:");
+                int scalar = Convert.ToInt32(Console.ReadLine());
+                int num1 = a * scalar;
+                int num2 = b * scalar;
+                int num3 = c * scalar;
+                int num4 = d * scalar;
+                Console.WriteLine($"\n{num1} {num2}");
+                Console.WriteLine($"{num3} {num4}");
+            }
+            else if (choice == "5")
+            {
+                int determinant = (a * d) - (b * c);
+                Console.WriteLine($"= {determinant}");
             }
             else
             {
